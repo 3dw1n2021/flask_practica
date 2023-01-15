b420aa2b906ab835dd5dbd192d237aec3285676e
Ed <ea.torresl1@uniandes.edu.co>
2023-01-15 13:27:55 -0500

    Importación BBDD

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,5 +1,10 @@
 from flaskr import create_app
+from .modelos import db, Cancion
 
 app = create_app('default')
 app_context = app.app_context()
 app_context.push()
+
+# importamos la base de datos -# 3
+db.init_app(app)
+db.create_all()
