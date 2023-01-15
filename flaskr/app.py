395637054efb1c87cc6bc91eed1eaeef00dd1dd2
Ed <ea.torresl1@uniandes.edu.co>
2023-01-15 13:44:06 -0500

    Prueba éxitosa BBDD Canción

--- flaskr/app.py
+++ flaskr/app.py
@@ -8,3 +8,13 @@ app_context.push()
 # importamos la base de datos -# 3
 db.init_app(app)
 db.create_all()
+
+
+#  PRUEBA
+with app.app_context():
+    c = Cancion(titulo = "Prueba", minutos = 2, segundos =25, interprete = "Juan Carlos" )
+    c2 = Cancion(titulo = "Excelente", minutos = 2, segundos =25, interprete = "Edwin Torres" )
+    db.session.add(c)
+    db.session.add(c2)
+    db.session.commit()
+    print(Cancion.query.all())
--- flaskr/modelos/modelos.py
+++ flaskr/modelos/modelos.py
@@ -8,3 +8,7 @@ class Cancion(db.Model):
     minutos = db.Column(db.Integer)
     segundos = db.Column(db.Integer)
     interprete = db.Column(db.String(128))
+
+#Prueba
+def __repr__(self):  # Representación
+    return "{}-{}-{}-{}".format(self.titulo, self.minutos, self.segundos, self.interprete)
