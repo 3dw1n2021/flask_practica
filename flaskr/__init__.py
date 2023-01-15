4e65adb73d49b1f34331fe569c8e06493a3ddf69
Ed <ea.torresl1@uniandes.edu.co>
2023-01-15 12:48:41 -0500

    Agregar libreria + Instanciar SQL + Folder modelos

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,4 +2,7 @@ from flask import Flask
 
 def create_app(config_name):
     app = Flask(__name__)
+    # Crear template de BBDD
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///practica.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # desactiva warnings
     return app
