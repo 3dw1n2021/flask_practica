645034f540b3b0be7e2b7784de5b65d0475a1050
Ed <ea.torresl1@uniandes.edu.co>
2023-01-18 23:10:12 -0500

    Prueba # 1

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,5 +1,5 @@
 from flaskr import create_app
-from .modelos import db, Cancion, Usuario, Album
+from .modelos import db, Cancion, Usuario, Album, Medio
 
 app = create_app('default')
 app_context = app.app_context()
@@ -8,3 +8,13 @@ app_context.push()
 # importamos la base de datos -# 3
 db.init_app(app)
 db.create_all()
+
+# PRUEBA
+with app.app_context():
+    u = Usuario(nombre = "Juan", contrasena = "12345")
+    a = Album(titulo = "prueba", anio =2000, descripcion = "texto", medio = Medio.CD )
+    u.albumes.append(a)
+    db.session.add(u)
+    db.session.commit()
+    print(Usuario.query.all())
+    print(Usuario.query.all()[0].albumes)
