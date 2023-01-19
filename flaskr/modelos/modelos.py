c41d5e36df9cccfe3c4c78a88e75473f850e13eb
Ed <ea.torresl1@uniandes.edu.co>
2023-01-18 23:48:03 -0500

    Relación Compartida: muchos(Album) - muchos(Canci)

--- flaskr/modelos/modelos.py
+++ flaskr/modelos/modelos.py
@@ -3,12 +3,17 @@ import enum
 
 db = SQLAlchemy()
 
+albumes_canciones = db.Table('album_cancion',
+    db.Column('album_id', db.Integer, db.ForeignKey('album.id'), primary_key = True),
+    db.Column('cancion_id', db.Integer, db.ForeignKey('cancion.id'), primary_key = True))
+
 class Cancion(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     titulo = db.Column(db.String(128))
     minutos = db.Column(db.Integer)
     segundos = db.Column(db.Integer)
     interprete = db.Column(db.String(128))
+    albumes = db.relationship('Album', secondary = 'album_cancion', back_populates = "canciones")
 
 class Medio(enum.Enum):
    DISCO = 1
@@ -22,6 +27,8 @@ class Album(db.Model):
     descripcion = db.Column(db.String(512))
     medio = db.Column(db.Enum(Medio))
     usuario = db.Column(db.Integer, db.ForeignKey("usuario.id"))
+    canciones = db.relationship('Cancion', secondary = 'album_cancion', back_populates = "albumes")
+    __table_args__ = (db.UniqueConstraint('usuario', 'titulo', name='titulo_unico_album'),)
 
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
