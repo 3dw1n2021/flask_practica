32f0d9a69ada46760881e5f6243ec3d6b86b9f3a
Ed <ea.torresl1@uniandes.edu.co>
2023-01-19 22:53:55 -0500

    Prueba: Generación de JSON Cancion

--- flaskr/app.py
+++ flaskr/app.py
@@ -8,3 +8,12 @@ app_context.push()
 # importamos la base de datos -# 3
 db.init_app(app)
 db.create_all()
+
+#PRUEBA 
+with app.app_context():
+
+    c = Cancion(titulo="Prueba", minutos=2, segundos=20, interprete="Sultanito")
+    c_schema = CancionSchema()
+    db.session.add(c)
+    db.session.commit()
+    print([c_schema.dump(c) for c in Cancion.query.all()])
