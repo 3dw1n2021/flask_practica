0152c33369b39a8d4e16e089b1af4ab421f090f1
Ed <ea.torresl1@uniandes.edu.co>
2023-01-19 22:31:12 -0500

    Importación de librería para generar JSONs

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,5 +1,5 @@
 from flaskr import create_app
-from .modelos import db, Cancion, Usuario, Album, enum
+from .modelos import db, Cancion, Usuario, Album, enum, CancionSchema
 
 app = create_app('default')
 app_context = app.app_context()
