b2874af1552c6156043fbcbcc5eafa0f7bbfedaa
Ed <ea.torresl1@uniandes.edu.co>
2023-01-28 14:37:30 -0500

    Declarar el parametro registrar_log

--- tareas/tareas.py
+++ tareas/tareas.py
@@ -2,7 +2,7 @@ from celery import Celery
 
 celery_app = Celery('tasks', broker='redis://localhost:6379/0')
 
-@celery_app.task() # 2: Decorador que hace esta función pase através de la cola
+@celery_app.task(name='registrar_log') # 1
 def registrar_log(usuario, fecha):
-    with open('log_signin.txt', 'a+') as file: # 1: añadir lineas; +: para añadir si no existe
+    with open('log_signin.txt', 'a+') as file: #
         file.write('{} - Inicio de sesion:{}\n'.format(usuario, fecha))
