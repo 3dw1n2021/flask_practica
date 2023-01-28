8ddbdb09687b48be37280d197743d3536fe8a037
Ed <ea.torresl1@uniandes.edu.co>
2023-01-28 15:58:22 -0500

    Estructura básica App

---
+++ microservicio_1/app.py
@@ -0,0 +1,10 @@
+from microservicio_1 import create_app
+from flask_restful import  Api
+
+app = create_app('default')
+app_context = app.app_context()
+app_context.push()
+
+api= Api(app)
+api.init_app(app)
+
